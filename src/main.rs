c20ff82b00746c7e63c2d5c9650a2f4997db618f
Pares <alfariz2004@gmail.com>
2025-04-12 14:13:08 +0700

    template string, variable

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
+use std::collections::HashMap;
+use handlebars::Handlebars;
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[test]
+fn test_handlebars() {
+    let mut handlebars = Handlebars::new();
+
+    handlebars.register_template_string("hello", "Hello, {{name}}!").unwrap();
+    handlebars.register_template_string("bye", "Bye, {{name}}").unwrap();
+
+    let mut data = HashMap::new();
+    data.insert("name", "ekotaro");
+
+    let rendered = handlebars.render("hello", &data).unwrap();
+    assert_eq!(rendered, "Hello, ekotaro!");
+
+    let rendered = handlebars.render("bye", &data).unwrap(); //
+    assert_eq!(rendered, "Bye, ekotaro");
+}
+
+#[test]
+fn test_nested_variable() {
+    let mut handlebars = Handlebars::new();
+
+    handlebars.register_template_string("hello", "Hello, {{person.first_name}} {{person.last_name}}").unwrap();
+
+    let mut data = HashMap::new();
+
+    let mut person = HashMap::new();
+    person.insert("first_name", "ekotaro");
+    person.insert("last_name", "kuroniwa");
+
+    data.insert("person", &person);
+
+    let rendered = handlebars.render("hello", &data).unwrap();
+    assert_eq!(rendered, "Hello, ekotaro kuroniwa");
+}
+
+#[test]
+fn test_html_escape() {
+    let mut handlebars = Handlebars::new();
+
+    handlebars.register_template_string("hello", "Hello, {{{name}}}!").unwrap();
+
+    let mut data = HashMap::new();
+    data.insert("name", "<p>ekotaro</p>");
+
+    let rendered = handlebars.render("hello", &data).unwrap();
+    assert_eq!(rendered, "Hello, <p>ekotaro</p>!");
+
 }
