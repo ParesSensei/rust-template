6ca6bf7c327071f7afbe41fc2568458ea4183c03
Pares <alfariz2004@gmail.com>
2025-04-14 21:50:36 +0700

    kelar templet

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,45 @@
-use std::collections::HashMap;
-use handlebars::Handlebars;
+use handlebars::{handlebars_helper,Handlebars, Helper, HelperDef, HelperResult, Output, RenderContext};
 use serde::Serialize;
 use serde_json::json;
+use std::collections::HashMap;
+
+
+struct DoubleNumber;
+
+impl HelperDef for DoubleNumber {
+    fn call<'reg: 'rc, 'rc>(
+        &self,
+        h: &Helper<'rc>,
+        r: &'reg Handlebars<'reg>,
+        ctx: &'rc handlebars::Context,
+        rc: &mut RenderContext<'reg, 'rc>,
+        out: &mut dyn Output,
+    ) -> HelperResult
+    {
+        let param = h.param(0).unwrap();
+        let number = param.value().as_i64().unwrap();
+        out.write(&format!("{}", number * 2))?;
+        Ok(())
+    }
+}
+
+#[derive(Serialize)]
+struct Address {
+    street: String,
+    city: String,
+}
 
 #[derive(Serialize)]
 struct Person {
     first_name: String,
     last_name: String,
+    hobbies: Vec<String>,
+    addresses: Vec<Address>,
 }
 
 #[derive(Serialize)]
-struct Data{
-    person: Person
+struct Data {
+    person: Person,
 }
 
 fn main() {
@@ -22,8 +50,12 @@ fn main() {
 fn test_handlebars() {
     let mut handlebars = Handlebars::new();
 
-    handlebars.register_template_string("hello", "Hello, {{name}}!").unwrap();
-    handlebars.register_template_string("bye", "Bye, {{name}}").unwrap();
+    handlebars
+        .register_template_string("hello", "Hello, {{name}}!")
+        .unwrap();
+    handlebars
+        .register_template_string("bye", "Bye, {{name}}")
+        .unwrap();
 
     let mut data = HashMap::new();
     data.insert("name", "ekotaro");
@@ -39,7 +71,9 @@ fn test_handlebars() {
 fn test_nested_variable() {
     let mut handlebars = Handlebars::new();
 
-    handlebars.register_template_string("hello", "Hello, {{person.first_name}} {{person.last_name}}").unwrap();
+    handlebars
+        .register_template_string("hello", "Hello, {{person.first_name}} {{person.last_name}}")
+        .unwrap();
 
     let mut data = HashMap::new();
 
@@ -57,7 +91,9 @@ fn test_nested_variable() {
 fn test_html_escape() {
     let mut handlebars = Handlebars::new();
 
-    handlebars.register_template_string("hello", "Hello, {{{name}}}!").unwrap();
+    handlebars
+        .register_template_string("hello", "Hello, {{{name}}}!")
+        .unwrap();
 
     let mut data = HashMap::new();
     data.insert("name", "<p>ekotaro</p>");
@@ -70,7 +106,9 @@ fn test_html_escape() {
 fn test_template_file() {
     let mut handlebars = Handlebars::new();
 
-    handlebars.register_template_file("hello", "templates/hello.mustache").unwrap();
+    handlebars
+        .register_template_file("hello", "templates/hello.mustache")
+        .unwrap();
 
     let mut data = HashMap::new();
     data.insert("name", "ekotaro");
@@ -83,7 +121,9 @@ fn test_template_file() {
 fn test_with() {
     let mut handlebars = Handlebars::new();
 
-    handlebars.register_template_file("hello", "templates/with-hello.mustache").unwrap();
+    handlebars
+        .register_template_file("hello", "templates/with-hello.mustache")
+        .unwrap();
 
     let mut data = HashMap::new();
 
@@ -101,13 +141,17 @@ fn test_with() {
 fn test_serde() {
     let mut handlebars = Handlebars::new();
 
-    handlebars.register_template_file("hello", "templates/with-hello.mustache").unwrap();
+    handlebars
+        .register_template_file("hello", "templates/with-hello.mustache")
+        .unwrap();
 
-    let data = Data{
-      person: Person{
-          first_name: "ekotaro".to_string(),
-          last_name: "kuroniwa".to_string()
-      }
+    let data = Data {
+        person: Person {
+            first_name: "ekotaro".to_string(),
+            last_name: "kuroniwa".to_string(),
+            hobbies: vec![],
+            addresses: vec![],
+        },
     };
 
     let rendered = handlebars.render("hello", &data).unwrap();
@@ -118,7 +162,9 @@ fn test_serde() {
 fn test_serde_json() {
     let mut handlebars = Handlebars::new();
 
-    handlebars.register_template_file("hello", "templates/with-hello.mustache").unwrap();
+    handlebars
+        .register_template_file("hello", "templates/with-hello.mustache")
+        .unwrap();
 
     let data = json!({
         "person": {
@@ -135,24 +181,38 @@ fn test_serde_json() {
 fn test_if() {
     let mut handlebars = Handlebars::new();
 
-    handlebars.register_template_file("hello", "templates/blog.mustache").unwrap();
+    handlebars
+        .register_template_file("layout/header", "templates/layout/header.mustache")
+        .unwrap();
+    handlebars
+        .register_template_file("layout/footer", "templates/layout/footer.mustache")
+        .unwrap();
+    handlebars
+        .register_template_file("hello", "templates/blog.mustache")
+        .unwrap();
 
     let data = json!({
-       "title": "belajar rust",
-        "content": "belajar rust dengan baik"
+        "title": "belajar rust",
+        "content": "belajar rust dengan baik",
+        "footer": "Programmer zaman now",
     });
 
     let rendered = handlebars.render("hello", &data).unwrap();
+    println!("{}", rendered);
+
     assert_eq!(rendered.contains("belajar rust"), true);
     assert_eq!(rendered.contains("belajar rust dengan baik"), true);
     assert_eq!(rendered.contains("Anonymous"), true);
+    assert_eq!(rendered.contains("Programmer zaman now"), true);
 }
 
 #[test]
 fn test_if2() {
     let mut handlebars = Handlebars::new();
 
-    handlebars.register_template_file("hello", "templates/blog.mustache").unwrap();
+    handlebars
+        .register_template_file("hello", "templates/blog.mustache")
+        .unwrap();
 
     let data = json!({
        "title": "belajar rust",
@@ -171,24 +231,125 @@ fn test_if2() {
 fn test_unless() {
     let mut handlebars = Handlebars::new();
 
-    handlebars.register_template_file("footer", "templates/footer.mustache").unwrap();
+    handlebars
+        .register_template_file("footer", "templates/footer.mustache")
+        .unwrap();
 
     let data = json!({});
 
     let rendered = handlebars.render("footer", &data).unwrap();
-    assert_eq!(rendered.contains("this content does not contains footer"), true);
+    assert_eq!(
+        rendered.contains("this content does not contains footer"),
+        true
+    );
 }
 
 #[test]
 fn test_unless2() {
     let mut handlebars = Handlebars::new();
 
-    handlebars.register_template_file("footer", "templates/footer.mustache").unwrap();
+    handlebars
+        .register_template_file("footer", "templates/footer.mustache")
+        .unwrap();
 
     let data = json!({
         "footer": "ekotaro"
     });
 
     let rendered = handlebars.render("footer", &data).unwrap();
-    assert_eq!(rendered.contains("this content does not contains footer"), false);
+    assert_eq!(
+        rendered.contains("this content does not contains footer"),
+        false
+    );
+}
+
+#[test]
+fn test_each() {
+    let mut handlebars = Handlebars::new();
+
+    handlebars
+        .register_template_file("person", "templates/person.mustache")
+        .unwrap();
+
+    let data = Person {
+        first_name: "ekotaro".to_string(),
+        last_name: "kuroniwa".to_string(),
+        hobbies: vec!["coding".to_string(), "decoding".to_string()],
+        addresses: vec![],
+    };
+
+    let rendered = handlebars.render("person", &data).unwrap();
+    assert_eq!(rendered.contains("ekotaro"), true);
+    assert_eq!(rendered.contains("kuroniwa"), true);
+    assert_eq!(rendered.contains("0 - coding"), true);
+    assert_eq!(rendered.contains("1 - decoding"), true);
+}
+
+#[test]
+fn test_each_object() {
+    let mut handlebars = Handlebars::new();
+
+    handlebars
+        .register_template_file("person", "templates/person.mustache")
+        .unwrap();
+
+    let data = Person {
+        first_name: "ekotaro".to_string(),
+        last_name: "kuroniwa".to_string(),
+        hobbies: vec!["coding".to_string(), "decoding".to_string()],
+        addresses: vec![
+            Address {
+                street: "jl. Kampung 1".to_string(),
+                city: "Jakarta".to_string(),
+            },
+            Address {
+                street: "jl. Kampung 2".to_string(),
+                city: "Bandung".to_string(),
+            },
+        ],
+    };
+
+    let rendered = handlebars.render("person", &data).unwrap();
+    println!("{}", rendered);
+
+    assert_eq!(rendered.contains("ekotaro"), true);
+    assert_eq!(rendered.contains("kuroniwa"), true);
+    assert_eq!(rendered.contains("0 - coding"), true);
+    assert_eq!(rendered.contains("1 - decoding"), true);
+    assert_eq!(rendered.contains("street - jl. Kampung 1"), true);
+    assert_eq!(rendered.contains("city - Jakarta"), true);
+    assert_eq!(rendered.contains("street - jl. Kampung 2"), true);
+    assert_eq!(rendered.contains("city - Bandung"), true);
+}
+
+#[test]
+fn test_helper() {
+    let mut handlebars = Handlebars::new();
+    handlebars.register_helper("double", Box::new(DoubleNumber));
+    handlebars.register_template_string("helper", "Result: {{double value}}").unwrap();
+
+    let data = json!({
+        "value": 10
+    });
+
+    let rendered = handlebars.render("helper", &data).unwrap();
+    assert_eq!(rendered.contains("Result: 20"), true);
+}
+
+handlebars_helper!(uppercase: |value: String| {
+    value.to_uppercase()
+});
+
+#[test]
+fn test_helper_macro() {
+    let mut handlebars = Handlebars::new();
+    handlebars.register_helper("uppercase", Box::new(uppercase));
+    handlebars.register_template_string("helper", "Hello : {{uppercase name}}").unwrap();
+
+    let data = json!({
+        "name": "eko"
+    });
+
+    let rendered = handlebars.render("helper", &data).unwrap();
+    assert_eq!(rendered.contains("Hello : EKO"), true);
 }
